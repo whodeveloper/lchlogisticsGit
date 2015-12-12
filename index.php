402af2a9b2df3809295f7652d4097920973319d0
whodeveloper <whodevelopthis@gmail.com>
2015-12-13 00:17:26 +0700

    Basic Template Halfway

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE>
+
+<html>
+	<head>
+		<title>LCH Logistic</title>
+		<meta charset = "utf-8">
+		<link rel = "stylesheet" type = "text/css" href = "css/style1.css">
+		<link rel = "stylesheet" type = "text/css" href = "">
+		<link rel = "stylesheet" type = "text/css" href = "">
+		<link rel = "" type = "" href = ""> 
+		<!-- ../something.php or ../../something.php locate the file that are on parent or granparent folder-->
+	</head>
+
+	<body>
+		<div class = "fullwrap">
+			<div class = "navbar">
+				<img src = "images/test.png" width = "40px" height = "40px" align="left">
+				<?php include ("navbar.php");?>
+			</div>
+
+			<div class = "maincontent">
+				<p><em>This is where the main <b>content</b> will be</em>
+				Content section A, up before 15<sup>th</sup> of January 2016.
+				</p>
+
+				<dl>
+					<dt>***TEST 1***</dt>
+					<dd>This is a test comment <br /> and here is another test comment</dd>
+					<br />
+					<dt>***TEST 2***</dt>
+					<dd>This is test content 2 <br /> this is another test content</dd>
+				</dl> 
+
+				<div class = "listception">
+					<ul>
+						<li>Delivery 1</li>
+							<ul>
+								<li>D.O 1.1</li>
+								<li>D.O 1.2</li>
+								<li>D.O 1.3</li>
+							</ul>
+						<li>Delivery 2</li>
+							<ul>
+								<li>D.O 2.1</li>
+								<li>D.O 2.2</li>
+								<li>D.O 2.3</li>
+							</ul>
+						<li>Delivery 3</li>
+							<ul>
+								<li>D.O 3.1</li>
+								<li>D.O 3.2</li>
+								<li>D.O 3.3</li>
+							</ul>
+					</ul> 
+				</div>
+			</div>
+
+			<div class = "footer">
+				<h3>LCH&#169</h3>
+				<address>
+					<p><a href = "mailto:sales@lchlogistic.com">contact us at sales@lchlogistic.com</a></p> 
+					<!-- mailto: can activate OS build in mailbox-->
+				</address>
+			</div>
+		</div>
+	</body>
+</html>
+
+<!-- ******** This is note section for future use********
+
+<div class = "samepage">
+	<a href = "#prome">Go To Prome </a>
+	<h2 id = "prome">This section is prome</h2>
+</div>
+	Using # to make the hyperlink go to certain section of the page and use ID on any HTML element to let
+	it now which section the hyperlink will go.  
+
+	<hr />
+		This is the create a straight line
+-->
+
---
+++ navbar.php
@@ -0,0 +1,6 @@
+<ul>
+	<li><a href = "index.php">Home</a></li>
+	<li><a href = "product.php">Product</a></li>
+	<li><a href = "contact.php">Contact</a></li>
+	<li><a href = "about_us.php">About Us</a></li>
+</ul>
