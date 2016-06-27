00af3e890d05109baf3b42379d9f25eb63e061e8
whodeveloper <whodevelopthis@gmail.com>
2016-06-27 20:22:43 +1000

    27/06/2016

---
+++ common/head.php
@@ -0,0 +1,11 @@
+<!--Title & Meta-->
+<title>Coorperate Title</title>
+<meta name="description" content="">
+<!--CSS-->
+<link rel="stylesheet" type="text/css" href="style/mainDesign.css">
+<link rel="stylesheet" type="text/css" href="style/mediaDesign.css">
+<!--Javascript-->
+<script src = "javascript/js.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!--Font-->
+<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
---
+++ common/header.php
@@ -0,0 +1,1 @@
+<h1>Cooperate Title</h1>
